6a2131b86860400d450fc4661165ae89d1821733
Liisi Kasuk <likasu@ttu.ee>
2020-08-14 11:59:48 +0300

    lisasin üldsätted

--- application/views/pages/editBuilding.php
+++ application/views/pages/editBuilding.php
@@ -15,7 +15,7 @@
 							<div id="asutus" class="tab-pane center  <?php if(!isset($data['type'])){ echo 'active';}else if($data['type']==1){echo 'active';}; ?>">
 							<?php echo form_open('building/update', array('id' => 'change')); ?>
 							<input class="d-none" type="hidden" name="id" value="<?php echo $this->uri->segment(3);?>">
-						
+
 									<h4 class="pt-2 txt-xl px-5 mx-5">Asutuse info</h4>
 
 									<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
@@ -54,25 +54,25 @@
 									<h4 class="mt-5 txt-xl px-5 mx-5 pb-3">Ruumid</h4>
 									<div class="form-label-group py-0 px-5 mx-5" id="saalid">
 											<label class="txt-regular txt-lg">Nähtavad ruumid <b data-tooltip="Kõik kasutajad näevad"><img id="tool" class="mr-5" src="<?php echo base_url(); ?>assets/img/icon-info.svg" width="3%"></b></label>
-											<?php foreach ($editBuildings as $value) { 
-										
-												if ($value['roomActive'] == 1) { 
-													echo('<div class="row d-flex mb-3 p-0 justify-content-between"> 
-													<input class="d-none" type="hidden" name="roomID[]" value="'.$value['id'].'"> 
+											<?php foreach ($editBuildings as $value) {
+
+												if ($value['roomActive'] == 1) {
+													echo('<div class="row d-flex mb-3 p-0 justify-content-between">
+													<input class="d-none" type="hidden" name="roomID[]" value="'.$value['id'].'">
 													<input class="form-control col-6" id="activeRoom[]" type="text" name="room[]" value="' . $value['roomName'] .'">
 													<input name="color[]" type="color" value="'. $value["roomColor"] .'">
-													<input type="button" id="activeOrPassive'.$value['id'].'" data-id="'.$value['id'].'" class="btn btn-custom btn-width-92 text-white text-center py-1 px-2 txt-strong" value="Nähtav"> 
+													<input type="button" id="activeOrPassive'.$value['id'].'" data-id="'.$value['id'].'" class="btn btn-custom btn-width-92 text-white text-center py-1 px-2 txt-strong" value="Nähtav">
 
 													<input data-id="'.$value['id'].'" class="btn btn-delete btn-width-92 text-white text-center py-1 px-2 txt-strong"  type="button" value="Kustuta">
-													</div>'); 
+													</div>');
 												}}; ?>
 									</div>
 									<div class="form-label-group py-0 px-5 mx-5">
 											<label class="txt-regular txt-lg">Peidetud ruumid <b data-tooltip="Nähtavad ainult asutuse kasutajatele"><img id="tool" class="mr-5" src="<?php echo base_url(); ?>assets/img/icon-info.svg" width="3%"></b></label>
-											<?php foreach ($editBuildings as $key => $value) { 
-												if ($value['roomActive'] == '0') { 
+											<?php foreach ($editBuildings as $key => $value) {
+												if ($value['roomActive'] == '0') {
 													echo('<div class="row d-flex mb-3 p-0 justify-content-between">
-													<input class="d-none" type="hidden" name="roomID[]" value="'.$value['id'].'"> 
+													<input class="d-none" type="hidden" name="roomID[]" value="'.$value['id'].'">
 													<input class="form-control col-6" id="inactiveRoom[]" type="text" name="room[]" value="' . $value['roomName'] .'">
 													<input name="color[]" type="color" value="'. $value["roomColor"] .'">
 													<input type="button" id="activeOrPassive' . $value['id']. '" data-id="'.$value['id'].'" class="btn btn-inactive btn-width-92 text-white text-center py-1 px-2 txt-strong" value="Peidus">
@@ -90,9 +90,9 @@
 											<button type="submit" class="btn btn-custom col-md-5 text-white txt-xl">Salvesta muudatused</button>
 									</div>
 							</form>
-							
-							</div>  
-							<div id="broneering" class="tab-pane center">   
+
+							</div>
+							<div id="broneering" class="tab-pane center">
 							<?php echo form_open('building/updateBookingSettings', array('id' => 'changebookingSettings')); ?>
 								<h4 class="pt-2 txt-xl px-5 mx-5">Administraatori broneeringuvormi vaikeseaded</h4>
 								<div class="row d-flex p-0 m-4 px-md-5 mx-5">
@@ -102,20 +102,20 @@
 								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
 									Klubi nimi  &nbsp;&nbsp;	<label><input type="checkbox" <?php if($bookingformdata['clubname_admin']){echo 'checked';}?>  name="clubname_admin" value="<?php echo $bookingformdata['clubname_admin'] ?>"><span></span></label>
 								</div>
-								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">	
+								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
 									Kontaktisik  &nbsp;&nbsp;	<label><input type="checkbox" <?php if($bookingformdata['contactname_admin']){echo 'checked';}?> name="contact_admin" value="<?php echo $bookingformdata['contactname_admin'] ?>"><span></span></label>
 								</div>
 									<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
 									Telefon  &nbsp;&nbsp;	<label><input type="checkbox" <?php if($bookingformdata['phone_admin']){echo 'checked';}?> name="phone_admin" value="<?php echo $bookingformdata['phone_admin'] ?>"><span></span></label>
-								</div>	
-								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">	
+								</div>
+								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
 									E-mail &nbsp;&nbsp;	<label><input type="checkbox" <?php if($bookingformdata['email_admin']){echo 'checked';}?> name="email_admin" value="<?php echo $bookingformdata['email_admin'] ?>"><span></span></label>
 								</div>	<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
 									Sündmus / Treeningu tüüp  &nbsp;&nbsp;	<label><input type="checkbox" <?php if($bookingformdata['type_admin']){echo 'checked';}?> name="type_admin" value="<?php echo $bookingformdata['type_admin'] ?>"><span></span></label>
 								</div>
-								
+
 								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
-								
+
 									<p class="txt-lg">	Vaikevärvid: </p>
 									&nbsp;&nbsp; <input type="color" id="favcolor" name="favcolor1" value="<?php echo $bookingformdata['color1'];?>">
 									&nbsp;&nbsp; <input type="color" id="favcolor" name="favcolor2" value="<?php echo $bookingformdata['color2'];?>">
@@ -129,33 +129,73 @@
 								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
 								</div>
 								<div class="bg-grey py-2">
-							
-								<h4 class="pt-2 txt-xl px-5 mx-5">Tavakasutaja broneeringuvormi vaikeseaded <b data-tooltip="Tavakasutajate broneering veel ei tööta ja see osa on arendamisel"><img id="tool" class="mr-5" src="<?php echo base_url(); ?>assets/img/icon-info.svg" width="3%"></b></h4>	
-								<div class="row d-flex p-0 m-4 px-md-5 mx-5">	
-									Luba tavakasutajate broneerimine  &nbsp;&nbsp;	<label><input type="checkbox" id="allowUserBooking" <?php if($bookingformdata['allow_booking']){echo 'checked';} ?> name="allowBooking" value="<?php echo $bookingformdata['allow_booking'] ?>"><span></span></label>
+
+									<h4 class="pt-2 txt-xl px-5 mx-5">Tavakasutaja broneeringuvormi vaikeseaded <b data-tooltip="Tavakasutajate broneering veel ei tööta ja see osa on arendamisel"><img id="tool" class="mr-5" src="<?php echo base_url(); ?>assets/img/icon-info.svg" width="3%"></b></h4>
+									<div class="row d-flex p-0 m-4 px-md-5 mx-5">
+										Luba tavakasutajate broneerimine  &nbsp;&nbsp;	<label><input type="checkbox" id="allowUserBooking" <?php if($bookingformdata['allow_booking']){echo 'checked';} ?> name="allowBooking" value="<?php echo $bookingformdata['allow_booking'] ?>"><span></span></label>
+									</div>
+									<div id="showOrHide">
+
+										<u><h4 class="px-5 mx-5">Üldsätted</h4></u>
+										<p class="pt-2 txt-lg px-5 mx-5">Kohustuslikud väljad tavakasutajatele: </p>
+
+										<div class="col-sm-8">
+											<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
+												Klubi nimi/korraldaja  &nbsp;&nbsp;	<label><input type="checkbox" class="form-check-input" <?php if($bookingformdata['clubname_user']){echo 'checked';} ?> name="clubname_user" value="<?php echo $bookingformdata['clubname_user'] ?>"><span></span></label>
+											</div>
+											<div class="row d-flex p-0 mt-3 px-md-5 mx-5">
+												Kontaktisik  &nbsp;&nbsp;	<label><input type="checkbox" class="form-check-input" <?php if($bookingformdata['contactname_user']){echo 'checked';} ?> name="name_user" value="<?php echo $bookingformdata['contactname_user'] ?>"><span></span></label>
+											</div>
+											<div class="row d-flex p-0 mt-3 px-md-5 mx-5">
+												Telefon  &nbsp;&nbsp;	<label><input type="checkbox"  <?php if($bookingformdata['phone_user']){echo 'checked';} ?> name="phone_user" value="<?php echo $bookingformdata['phone_user'] ?>"><span></span></label>
+											</div>
+											<div class="row d-flex p-0 mt-3 px-md-5 mx-5">
+												E-mail &nbsp;&nbsp;	<label><input type="checkbox"  <?php if($bookingformdata['email_user']){echo 'checked';} ?> name="email_user" value="<?php echo $bookingformdata['email_user'] ?>"><span></span></label>
+											</div>
+											<div class="row d-flex p-0 mt-3 px-md-5 mx-5">
+												Ruumi kasutamise eesmärk  &nbsp;&nbsp;	<label><input type="checkbox"  <?php if($bookingformdata['type_user']){echo 'checked';} ?> name="type_user" value="<?php echo $bookingformdata['type_user'] ?>"><span></span></label>
+											</div>
+										</div>
+										<p class="pt-4 txt-lg px-5 mx-5">Maksevõimalused Teie asutuses: </p>
+										<div class="col-sm-8">
+										<div class="row d-flex p-0 mt-3 px-md-5 mx-5">
+											<label><input type="checkbox"  <?php if($bookingformdata['type_user']){echo 'checked';} ?> name="type_user" value="<?php echo $bookingformdata['type_user'] ?>"><span></span></label> &nbsp;&nbsp;	 Sularaha kohapeal tasudes
+										</div>
+										<div class="row d-flex p-0 mt-2 px-md-5 mx-5">
+											<label><input type="checkbox"  <?php if($bookingformdata['type_user']){echo 'checked';} ?> name="type_user" value="<?php echo $bookingformdata['type_user'] ?>"><span></span></label> &nbsp;&nbsp;	 Pangakaart kohapeal tasudes
+										</div>
+										<div class="row d-flex p-0 mt-2 px-md-5 mx-5">
+											<label><input type="checkbox"  <?php if($bookingformdata['type_user']){echo 'checked';} ?> name="type_user" value="<?php echo $bookingformdata['type_user'] ?>"><span></span></label> &nbsp;&nbsp;	 Arve
+										</div>
+										<div class="row d-flex p-0 mt-2 px-md-5 mx-5">
+											<label><input type="checkbox"  <?php if($bookingformdata['type_user']){echo 'checked';} ?> name="type_user" value="<?php echo $bookingformdata['type_user'] ?>"><span></span></label> &nbsp;&nbsp;	 Ettemaks
+										</div>
+										<div class="row d-flex p-0 mt-2 px-md-5 mx-5">
+											<label><input type="checkbox"  <?php if($bookingformdata['type_user']){echo 'checked';} ?> name="type_user" value="<?php echo $bookingformdata['type_user'] ?>"><span></span></label> &nbsp;&nbsp;	 Muu
+										</div>
+										</div>
+										<p class="pt-4 txt-lg px-5 mx-5">Kasutustingimused: </p>
+										<div class="col-sm-8">
+										<div class="row d-flex p-0 mt-3 px-md-5 mx-5 col-12">
+											<label><input type="checkbox"  <?php if($bookingformdata['type_user']){echo 'checked';} ?> name="type_user" value="<?php echo $bookingformdata['type_user'] ?>"><span></span></label> &nbsp;&nbsp;	 Soovin, et klient peab nõustuma kasutustingimustega
+										</div>
+										
+										<p class="pt-3 txt-lg px-5 mx-5">Sisesta siia oma asutuse kasutustingimused: </p>
+										
+										<div class="form-label-group mt-2 pb-2 px-5 mx-5">
+											<div class="input-group">
+												<textarea class="form-control"></textarea>
+											</div>
+										</div>
+										
+									</div>
+
 								</div>
-								<div id="showOrHide">
-									<p class="pt-2 txt-lg px-5 mx-5">Kohustuslikud väljad tavakasutajatele: </p>
-					
-								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
-									Klubi nimi  &nbsp;&nbsp;	<label><input type="checkbox"  <?php if($bookingformdata['clubname_user']){echo 'checked';} ?> name="clubname_user" value="<?php echo $bookingformdata['clubname_user'] ?>"><span></span></label>
-								</div>
-								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">	
-									Kontaktisik  &nbsp;&nbsp;	<label><input type="checkbox"  <?php if($bookingformdata['contactname_user']){echo 'checked';} ?> name="name_user" value="<?php echo $bookingformdata['contactname_user'] ?>"><span></span></label>
-								</div>
-									<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
-									Telefon  &nbsp;&nbsp;	<label><input type="checkbox"  <?php if($bookingformdata['phone_user']){echo 'checked';} ?> name="phone_user" value="<?php echo $bookingformdata['phone_user'] ?>"><span></span></label>
-								</div>	
-								<div class="row d-flex p-0 mt-4 px-md-5 mx-5">	
-									E-mail &nbsp;&nbsp;	<label><input type="checkbox"  <?php if($bookingformdata['email_user']){echo 'checked';} ?> name="email_user" value="<?php echo $bookingformdata['email_user'] ?>"><span></span></label>
-								</div>	<div class="row d-flex p-0 mt-4 px-md-5 mx-5">
-									Sündmus / Treeningu tüüp  &nbsp;&nbsp;	<label><input type="checkbox"  <?php if($bookingformdata['type_user']){echo 'checked';} ?> name="type_user" value="<?php echo $bookingformdata['type_user'] ?>"><span></span></label>
-								</div>
+
 							</div>
-						
-							<div class="row d-flex p-0 mt-4 px-md-5 mx-5">	
-							</div>
-							</div>
+
+
+
 
 							<div class="d-flex justify-content-end my-5 px-5 mx-5">
 										<a class="txt-xl link-deco align-self-center py-0 pr-5 mr-2" href="<?php echo base_url(); ?>building/view/<?php  print_r($this->session->userdata['building']);?>">Katkesta</a>
@@ -171,26 +211,28 @@
                     <?php } else { redirect(''); }?>
 
 
-                             
+
 <script>
 
 $( document ).ready(function() {
 
-	if(!$('#allowUserBooking').is(":checked")){
-			$( "#showOrHide" ).hide();
-		}
+	// if(!$('#allowUserBooking').is(":checked")){
+	// 		$( "#showOrHide" ).hide();
+	// 	}
 
-	$(':checkbox').click(function() {
-   		value = +$(this).is( ':checked' );
-		   $(this).val(value);
-		
-		if($('#allowUserBooking').is(":checked")){
-			console.log(value);
-			$( "#showOrHide" ).show();
-		}else{
-			$( "#showOrHide" ).hide();
-		}
-	});
+	// $(':checkbox').click(function() {
+   	// 	value = +$(this).is( ':checked' );
+	// 	   $(this).val(value);
+
+	// 	if($('#allowUserBooking').is(":checked")){
+	// 		console.log(value);
+	// 		$( "#showOrHide" ).show();
+	// 	}else{
+	// 		$( "#showOrHide" ).hide();
+	// 	}
+	// });
+
+
 
 
 	var counter=1;
@@ -199,9 +241,9 @@ $( document ).ready(function() {
 	counter++;
   });
 
-  $(document).on('click', '.abc', function() { 
-	
-	  $(this).parent().remove(); 
+  $(document).on('click', '.abc', function() {
+
+	  $(this).parent().remove();
 	  });
 
 
@@ -215,25 +257,25 @@ $( document ).ready(function() {
       dataType: "html"
     }).done(function( msg ) {
 
-			if(msg=='""'){  
-				elementToDelete.parent().remove(); 
+			if(msg=='""'){
+				elementToDelete.parent().remove();
 			}
 			else{
 			$( "#textMessageToUser" ).append('<p class="alert alert-danger text-center">'+msg+'</p>');
 		window.setTimeout(function() {
                 $(".alert").fadeTo(500, 0).slideUp(500, function(){
-                    $(this).remove(); 
+                    $(this).remove();
                 });
 			}, 4000);}
-	
+
     }).fail(function( jqXHR, textStatus ) {
       alert( "Request failed: " + textStatus );
-    }); 
+    });
   });
 
- 
-  
-  $(document).on('click', '.addedRoom', function() { 
+
+
+  $(document).on('click', '.addedRoom', function() {
 
 
 		var newRoomStatus=1;
@@ -243,8 +285,8 @@ $( document ).ready(function() {
 		$(this).removeClass("btn-custom");
 		$(this).addClass("btn-inactive");
 		newRoomStatus=0;
-		
-	} 
+
+	}
 	else{
 		$(this).val("Nähtav");
 		$(this).next().val('1');
@@ -263,20 +305,20 @@ $( document ).ready(function() {
 		$(this).removeClass("btn-custom");
 		$(this).addClass("btn-inactive");
 		roomStatus=0;
-		
-	} 
+
+	}
 	else{
 		$(this).val("Nähtav");
 		$(this).removeClass("btn-inactive");
 		$(this).addClass("btn-custom");
 		roomStatus=1;
 	}
-	
+
 	var elementToDelete=$(this);
     $.ajax({
 	  url: "<?php echo base_url(); ?>building/roomStatus",
       method: "POST", // use "GET" if server does not handle DELETE
-      data: { 
+      data: {
 		"roomID": $(this).data("id"),
 	    "roomStatus": roomStatus
 		 },
@@ -288,7 +330,7 @@ $( document ).ready(function() {
 			}
     }).fail(function( jqXHR, textStatus ) {
       alert( "Request failed: " + textStatus );
-    }); 
+    });
   });
 
 
